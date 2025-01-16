07ce9d1973c557eec86d36b91579848f83840eb8
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-01-16 20:15:39 +0500

    Update multiply odd indexed elements by 2 and add 10 to the even elements.c

--- Arrays_Practice/multiply odd indexed elements by 2 and add 10 to the even elements.c
+++ Arrays_Practice/multiply odd indexed elements by 2 and add 10 to the even elements.c
@@ -4,7 +4,7 @@ int main(){
 	int arr[10]={11,22,33,44,55,66,77,88,99,110};
 	for(int i=0;i<10;i++){
 		int odd=1;
-	  int even=0;
+	        int even=0;
 		if(i%2!=0){
 		odd=arr[i]*2;
 		printf("%d is odd index so multiply %d by 2 gives %d\n",i,arr[i],odd);
