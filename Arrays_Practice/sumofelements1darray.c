4d06bc9a382b58ef46a90df1209d15fb1b90b8b1
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-01-16 08:22:52 +0500

    Update sumofelements1darray.c

--- Arrays_Practice/sumofelements1darray.c
+++ Arrays_Practice/sumofelements1darray.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+int main(){
+int arr[10]={10,20,30,40,50,60,70,80,90,100};
+int sum=0;
+for(int i=0;i<10;i++){
+  sum=sum+arr[i];
+}
+printf("The sum of elements of array is:%d",sum);
+return 0;
+}
