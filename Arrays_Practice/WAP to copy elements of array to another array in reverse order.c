ba1bd9db09425da07936df75b02cb07bd2d13329
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-01-17 01:51:15 +0500

    Update WAP to copy elements of array to another array in reverse order.c

--- Arrays_Practice/WAP to copy elements of array to another array in reverse order.c
+++ Arrays_Practice/WAP to copy elements of array to another array in reverse order.c
@@ -1,1 +1,11 @@
-
+//WAP to copy elements of array to another array in reverse order.
+#include<stdio.h>
+int main(){
+	int arr[8]={1,2,3,4,5,6,7,8};
+	int brr[8];
+	for(int i=0;i<8;i++){
+		brr[i]=arr[7-i];
+		printf("%d ",brr[i]);
+	}
+	return 0;
+}
