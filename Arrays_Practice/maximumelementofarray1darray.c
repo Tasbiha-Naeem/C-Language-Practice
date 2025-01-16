9d9e4d4b1257b45f91f8276f0d72051c28fe375f
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-01-16 19:30:00 +0500

    Update maximumelementofarray1darray.c

--- Arrays_Practice/maximumelementofarray1darray.c
+++ Arrays_Practice/maximumelementofarray1darray.c
@@ -1,1 +1,18 @@
-
+//printf the maximum element of array.
+#include<stdio.h>
+#include<climits>
+int main(){
+  int arr[10];
+  int maximum_element=INT_MIN;
+  printf("Enter the elements of array:\n");
+  for(int i=0;i<10;i++){
+  	scanf("%d",&arr[i]);
+  }
+  for(int i=0;i<10;i++){
+  	if(maximum_element<arr[i]){
+  		maximum_element=arr[i];
+	  }
+  }
+  printf("The maximum element of array is %d.",maximum_element);
+  return 0;
+}
