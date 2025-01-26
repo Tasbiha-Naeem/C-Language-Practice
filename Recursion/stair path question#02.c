45c80f2a9e157f00411a0ff75d8b4ec2f4b7f46c
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-01-27 03:46:40 +0500

    Update stair path question#02.c

--- Recursion/stair path question#02.c
+++ Recursion/stair path question#02.c
@@ -3,7 +3,7 @@ int stair(int n){
 	if(n==1) return 1;
 	if(n==2) return 2;
 	if(n==3) return 4;
-	int totalWays=stair(n-1)+stair(n-2)+(n-3);
+	int totalWays=stair(n-1)+stair(n-2)+stair(n-3);
 	return totalWays;
 }
 int main(){
