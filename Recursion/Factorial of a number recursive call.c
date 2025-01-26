6231dc7b468f9b1cf035166511b23ecb65e1f4e7
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-01-26 17:43:40 +0500

    Update Factorial of a number recursive call.c

--- Recursion/Factorial of a number recursive call.c
+++ Recursion/Factorial of a number recursive call.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+ int factorial(int n){
+ 	if(n==1 || n==0){
+ 		return 1;
+	 }
+ 	return n*factorial(n-1);
+ }
+int main(){
+	int num;
+	printf("Enter the number:\n");
+	scanf("%d",&num);
+	int fact=factorial(num);
+	printf("Factorial of %d is:%d",num,fact);
+	return 0;
+}
