896ca1a1e192eeb8652263cc53b46209c562dbc1
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-02-07 20:34:30 +0500

    Create percentage of 5 subjects.c

---
+++ Variables,Operators,Input and Output/percentage of 5 subjects.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  float m1=98;
+  float m2=78;
+  float m3=88;
+  float m4=67;
+  float m5=33;
+  float percentage=(m1+m2+m3+m4+m5)/5;
+  printf("%f\n",percentage);
+  return 0;
+}
