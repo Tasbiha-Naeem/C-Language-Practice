7cbac9d72f1603162df7cfe0c397718e27245a2d
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-02-08 00:18:30 +0500

    Create data types.c

---
+++ Variables,Operators,Input and Output/data types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int x = 5;
+    char ch = 'a';
+    float r = 3.14;
+    double y = 8233.32938;
+    long n = 728239743;
+    long long c = 743989832497427293; 
+    short t = 32768;  
+    return 0;
+}
