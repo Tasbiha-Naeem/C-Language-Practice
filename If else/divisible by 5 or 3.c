df35d016a4a3401d5759be8cf098b3faf6cb7004
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-02-08 21:08:03 +0500

    Create divisible by 5 or 3.c

---
+++ If else/divisible by 5 or 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int num;
+	printf("Enter the number:\n");
+	scanf("%d",&num);
+	if(num%5==0  || num%3==9){
+		printf("%d is divisible by 5 or 3.\n",num);
+	}
+	else{
+		printf("%d is not divisible by 5 or 3.\n",num);
+	}
+	return 0;
+}
