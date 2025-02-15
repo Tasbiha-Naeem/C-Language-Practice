ce2c0fec5e4fc64032a80b3f53092839e5e23efa
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-02-15 17:10:48 +0500

    Update divisible by 5 or 3.c

--- If else/divisible by 5 or 3.c
+++ If else/divisible by 5 or 3.c
@@ -3,7 +3,7 @@ int main(){
 	int num;
 	printf("Enter the number:\n");
 	scanf("%d",&num);
-	if(num%5==0  || num%3==9){
+	if(num%5==0  || num%3==0){
 		printf("%d is divisible by 5 or 3.\n",num);
 	}
 	else{
