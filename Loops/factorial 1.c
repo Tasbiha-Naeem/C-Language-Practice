8c69e02a409a1468e049f5a1bec97406012c0c57
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-02-21 17:20:09 +0500

    Create factorial 1.c

---
+++ Loops/factorial 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int number;
+	printf("Enter the number:\n");
+	scanf("%d",&number);
+	int factorial;
+	int product=1;
+	for(int i=1;i<=number;i++){
+		product=product*i;
+	}
+	printf("The factorial of %d is %d",number,product);
+	
+	return 0;
+}
