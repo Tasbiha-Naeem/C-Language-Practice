1e0c2321406fe7e69d0871c748da4537de0726f3
Tasbiha Naeem <tasbihanaeem123@gmail.com>
2025-02-22 20:06:42 +0500

    Create pattern 1.c

---
+++ Pattern Printing/pattern 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int n,m;
+	printf("Enter the number of rows:\n");
+	scanf("%d",&n);
+	printf("Enter the number of columns:\n");
+	scanf("%d",&m);
+	for(int i=1;i<=n;i++){
+		for(int i=1;i<=m;i++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
